42f3b861eee29aa0c2a54e4790ffa23ac366e0d2
Ryan Schlesinger <ryan@ryanschlesinger.com>
2016-04-12 22:18:22 -0700

    Requiring the gem pulls in top level service objects

--- lib/consul_heartbeat.rb
+++ lib/consul_heartbeat.rb
@@ -1,5 +1,6 @@
-require "consul_heartbeat/version"
+require 'consul_heartbeat/version'
+require 'consul_heartbeat/start_heartbeat'
+require 'consul_heartbeat/beat_once'
 
 module ConsulHeartbeat
-  # Your code goes here...
 end
