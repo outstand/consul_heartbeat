4b93d957120d3ea79e7db09fcbbc08ec1e5b9c06
Ryan Schlesinger <ryan@ryanschlesinger.com>
2016-04-12 22:17:59 -0700

    Add comment for future work

--- lib/consul_heartbeat/get_private_ip.rb
+++ lib/consul_heartbeat/get_private_ip.rb
@@ -3,6 +3,7 @@ require 'consul_heartbeat/base'
 module ConsulHeartbeat
   class GetPrivateIP < Base
     def call
+      # http://169.254.169.254/latest/meta-data/local-ipv4
       puts 'get private ip'
       OpenStruct.new(
         private_ip: '1.2.3.4'
